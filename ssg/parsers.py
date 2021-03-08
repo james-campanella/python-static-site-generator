284a3acc022697c6f3936492b0d85aaf3aa5b2a9
James F. Campanella <jamessc@siu.edu>
2021-03-08 15:33:24 -0600

    Renamed fp to file in the with statements, also corrected syntax errors of missing colons in site.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -12,13 +12,13 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path: Path):
-        with open(path, 'r') as fp:
-            return fp.readlines()
+        with open(path, 'r') as file:
+            return file.readlines()
 
     def write(self, path: Path, dest: Path, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path, 'w') as fp:
-            fp.write(content)
+        with open(full_path, 'w') as file:
+            file.write(content)
 
     def copy(self, path: Path, source: Path, dest: Path):
         shutil.copy2(path, dest / path.relative_to(source))
